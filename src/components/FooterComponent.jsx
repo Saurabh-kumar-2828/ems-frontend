117ff015af5430bd8d4faa2b8aa169ebb4c9da8d
saura-kumar <iamsaurabhsharma111@gmail.com>
2024-08-21 03:01:31 +0530

    Employee management project frontend..

---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+const FooterComponent = () => {
+    return(
+        <div>
+            <footer className="footer">
+               <span>ALL rights reserved 2024 by saurabh kumar</span>
+            </footer>
+        </div>
+    )
+}
+
+export default FooterComponent
